8e08eb8278068c1144196b3b988dcdddf9f33728
Talitha Souza <litha06h@gmail.com>
2025-05-23 14:30:12 -0300

    git init: migração de repositorio

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -13,7 +13,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 namespace application
